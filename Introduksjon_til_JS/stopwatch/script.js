0666fc0da323f9ec5a355c1f7aa32c012a99980e
MiiNK1Y <98641699+MiiNK1Y@users.noreply.github.com>
2024-08-16 12:52:00 +0200

    ..

--- Introduksjon_til_JS/stopwatch/script.js
+++ Introduksjon_til_JS/stopwatch/script.js
@@ -1,14 +1,42 @@
-var time = 1;
+let time = 0;
+let timer_running = false;
+let round_time;
+
+var my_timer;
+
+let display_round = document.getElementById("display-round");
+let show_round = document.getElementById("show-round");
+let show_seconds = document.getElementById("seconds");
 
 function addSecond() {
-    document.getElementById("seconds").innerHTML = time;
     time++;
+    show_seconds.innerHTML = time;
 }
 
-function timer() {
-    const my_timer = setInterval(addSecond, 1000);
+function timerStart() {
+    if (!timer_running) {
+        timer_running = true;
+        my_timer = setInterval(addSecond, 1000);
+    }
 }
 
 function timerStop() {
-    clearInterval(my_timer);
+    if (timer_running) {
+        clearInterval(my_timer);
+        timer_running = false;
+
+        time = 0;
+        round_time = 0;
+        show_seconds.innerHTML = time;
+        display_round.style.display = "none";
+    }
+}
+
+function timerRound() {
+    round_time = time;
+    display_round.style.display = "flex";
+    show_round.innerHTML = round_time;
+
+    time = 0;
+    show_seconds.innerHTML = time;
 }
