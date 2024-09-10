3e23c266474779f71b269c5e46972c7968fa791b
MiiNK1Y <98641699+MiiNK1Y@users.noreply.github.com>
2024-09-10 17:46:31 +0200

    further development

--- loops_&_arrays/magic_8_ball/script.js
+++ loops_&_arrays/magic_8_ball/script.js
@@ -1,33 +1,65 @@
 // model
 const app = document.getElementById("app");
+const appMask = document.getElementById("app-mask");
 
-// always visible
-let eightBallSlider = /*HTML*/ `
-    <div id="eight_ball" onclick="showTextButton()">
-        <div id="eight_ball__slider" class="eight_ball__slider_front">
-            <div id="eight_ball__front"><strong>8</strong></div>
-            <div id="eight_ball__back">
-                <img src="triangle.png" id="triangle" class="triangle_hide" />
-                <div id="text">Hail Satan</div>
-            </div>
-        </div>
-    </div>
-    `;
+const messageList = [
+                "Hail Satan.", 
+                "Kill your<br>grandma.", 
+                "Ligma<br>Balls.",
+                "The fuck is<br>wrong with<br>you?",
+                "Don't know.",
+                "Epstein didn't<br>kill<br>himself.",
+                "Only you can<br>stop forest<br>fires.",
+                "Suicide?"
+                ];
 
 
 // view
 updateView();
 function updateView() {
     app.innerHTML = /*HTML*/ `
-        ${eightBallSlider}
+        ${eightBallSliderDiv()}
     `;
+}
+
+function eightBallSliderDiv() {
+    let eightBallSlider = /*HTML*/ `
+        <div id="eight_ball" class="clickable" onclick="showTextButton()">
+            <div id="eight_ball__slider" class="eight_ball__slider_front">
+                <div id="eight_ball__front"><strong>8</strong></div>
+                <div id="eight_ball__back">
+                    <img src="triangle.png" id="triangle" class="triangle_hide" />
+                    <div id="text">${setMessage()}</div>
+                </div>
+            </div>
+        </div>
+    `;
+    
+    return eightBallSlider;
 }
 
 
 // controller
+function setMessage() {
+    return messageList[Math.floor(Math.random() * messageList.length)]
+}
+
+function showAppMask() {
+    appMask.style.display = "block";
+}
+
+function hideAppMask() {
+    appMask.style.display = "none";
+}
+
 function showTextButton() {
+    setMessage();
+    updateView();
+
+    let ball = document.getElementById("eight_ball").classList;
     let curBallClass = document.getElementById("eight_ball__slider").classList;
     let curTriangleClass = document.getElementById("triangle").classList;
+
     if (
         curBallClass.contains("eight_ball__slider_front") ||
         curTriangleClass.contains("triangle_hide")
@@ -36,6 +68,14 @@ function showTextButton() {
         curBallClass.add("eight_ball__slider_back");
         curTriangleClass.remove("triangle_hide");
         curTriangleClass.add("triangle_show");
+        ball.remove("clickable")
+        ball.add("no_click");
+        showAppMask();
+        setTimeout(function() {
+            ball.remove("no_click")
+            ball.add("clickable")
+            hideAppMask();
+        }, 2500);
     } else if (
         curBallClass.contains("eight_ball__slider_back") ||
         curTriangleClass.contains("triangle_show")
@@ -44,5 +84,13 @@ function showTextButton() {
         curBallClass.add("eight_ball__slider_front");
         curTriangleClass.remove("triangle_show");
         curTriangleClass.add("triangle_hide");
+        ball.remove("clickable")
+        ball.add("no_click");
+        showAppMask();
+        setTimeout(function() {
+            ball.remove("no_click")
+            ball.add("clickable")
+            hideAppMask();
+        }, 2500);
     };
 }
