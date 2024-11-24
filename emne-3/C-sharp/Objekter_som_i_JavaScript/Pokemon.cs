9cd27e016b4f3194a6dcf950371ecf64d7dda5cf
MiiNK1Y <98641699+MiiNK1Y@users.noreply.github.com>
2024-11-24 18:48:42 +0100

    restructuring directories

---
+++ emne-3/C-sharp/Objekter_som_i_JavaScript/Pokemon.cs
@@ -0,0 +1,10 @@
+namespace Objekter_som_i_JavaScript
+{
+    class Pokemon
+    {
+        public string? Type;
+        public string? Name;
+        public int Health;
+        public int Weight;
+    }
+}
---
+++ emne-3/C-sharp/Objekter_som_i_JavaScript/Program.cs
@@ -0,0 +1,13 @@
+﻿using Objekter_som_i_JavaScript;
+
+Pokemon Pikachu = new Pokemon
+{
+    Name = "Bigrig",
+    Type = "Lightning",
+    Health = 100,
+    Weight = 11 // Kg
+};
+
+Console.WriteLine(Pikachu.Name);
+Console.WriteLine(Pikachu.Type);
+Console.WriteLine(Pikachu.Health);
